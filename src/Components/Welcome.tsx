5c9d0fa348e3367cf57280ca0a3963dc1b63c632
Paulo Vitor <paulovitor2123@gmail.com>
2022-02-22 01:37:10 -0300

    first commit

---
+++ src/Components/Welcome.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+interface Props {
+  title: string;
+}
+export function Welcome({ title }: Props) {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  );
+}
