a8bc828266df568712145d6f68172b463459c5c6
Paulo Vitor <paulovitor2123@gmail.com>
2022-02-24 01:16:10 -0300

    feat: Add responsive size and doing header

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  Photo,
+  User,
+  UserGreeting,
+  UserInfo,
+  UserName,
+  UserWrapper,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/47464843?s=400&u=f551614c8a077057a346680d3b99bb8e534d8f46&v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Paulo Vitor</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
