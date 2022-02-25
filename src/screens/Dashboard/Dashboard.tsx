d21229aa4b1eb19ac15d64635b6a424727a11419
Paulo Vitor <paulovitor2123@gmail.com>
2022-02-25 02:11:54 -0300

    feat: Add highlightCards in Header

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HighlightCard } from "../../components/HighlightCard";
 import {
   Container,
   Header,
@@ -8,6 +9,8 @@ import {
   UserInfo,
   UserName,
   UserWrapper,
+  Icon,
+  HighlightCards,
 } from "./styles";
 
 export function Dashboard() {
@@ -26,8 +29,29 @@ export function Dashboard() {
               <UserName>Paulo Vitor</UserName>
             </User>
           </UserInfo>
+          <Icon name="power" />
         </UserWrapper>
       </Header>
+      <HighlightCards>
+        <HighlightCard
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+          type="up"
+        />
+        <HighlightCard
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+          type="down"
+        />
+        <HighlightCard
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+          type="total"
+        />
+      </HighlightCards>
     </Container>
   );
 }
